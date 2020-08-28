7f6acad6d7fc9e2ae9a9c90d0bd1c672e8f7ad8c
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2020-08-28 19:01:37 +0100

    getting some weird test failures trying to add more tests to client_test...

--- sqs/client_test.go
+++ sqs/client_test.go
@@ -10,6 +10,10 @@ import (
 	fakeClient "github.com/alphagov/paas-sqs-broker/sqs/fakes"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/awslabs/goformation"
+	goformationt "github.com/awslabs/goformation/v4/cloudformation"
+	goformationsqs "github.com/awslabs/goformation/v4/cloudformation/sqs"
+	goformationtags "github.com/awslabs/goformation/v4/cloudformation/tags"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	uuid "github.com/satori/go.uuid"
@@ -76,7 +80,65 @@ var _ = Describe("Client", func() {
 		})
 	})
 
-	It("Does a thing", func() {
-		Expect(true).To(Equal(true))
+	Describe("CreateStack", func() {
+		It("constructs the full stack when creating", func() {
+			instanceID := uuid.NewV4().String()
+			orgID := uuid.NewV4().String()
+			params := sqs.QueueParams{}
+			sqsClient.CreateStack(context.Background(), instanceID, orgID, params)
+			Expect(cfnAPI.CreateStackWithContextCallCount()).To(Equal(1))
+			_, input, _ := cfnAPI.CreateStackWithContextArgsForCall(0)
+			Expect(*input.StackName).To(Equal(fmt.Sprintf("test-queue-prefix-%s", instanceID)))
+
+			Expect(input.Capabilities).To(ConsistOf(
+				aws.String("CAPABILITY_NAMED_IAM"),
+			))
+			Expect(input.TemplateBody).ToNot(BeNil())
+
+			t, err := goformation.ParseYAML([]byte(*input.TemplateBody))
+			Expect(err).ToNot(HaveOccurred())
+
+			// we should see a queue resource
+			// TODO WIP what is not working here?
+			Expect(t.Resources[sqs.SQSResourceName]).To(BeAssignableToTypeOf(&goformationsqs.Queue{}))
+			v := make([]goformationt.Resource, 0, len(t.Resources))
+			for  _, value := range t.Resources {
+				v = append(v, value)
+			}
+			Expect(v).To(ContainElement(BeAssignableToTypeOf(&goformationsqs.Queue{})))
+			queue, ok := t.Resources[sqs.SQSResourceName].(*goformationsqs.Queue)
+			Expect(ok).To(BeTrue())
+
+			// fifo should be set to false because we asked for a standard queue
+			Expect(queue.FifoQueue).To(BeFalse())
+
+			// should have suitable tags set
+			Expect(queue.Tags).To(And(
+				ContainElement(goformationtags.Tag{
+					Key:   "Name",
+					Value: instanceID,
+				}),
+				ContainElement(goformationtags.Tag{
+					Key:   "Service",
+					Value: "sqs",
+				}),
+				ContainElement(goformationtags.Tag{
+					Key:   "Customer",
+					Value: orgID,
+				}),
+				ContainElement(goformationtags.Tag{
+					Key:   "Environment",
+					Value: "test",
+				}),
+			))
+
+			// we set this in provdata
+			Expect(queue.ContentBasedDeduplication).To(BeTrue())
+
+		})
+	})
+
+	Describe("GetStackStatus", func() {
+		// TODO
 	})
 })
